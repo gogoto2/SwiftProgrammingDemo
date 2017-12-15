1e143a553c0fa1de36071a0ef09e96abe4e3012b
MengXianglei <mengxianglei@gezlife.com>
2017-12-15 18:24:19 +0800

    Variable

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,7 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
+import Cocoa
 
-var str = "Hello, playground"
+var str = "Swift Programming Demo "
+str += "!"
+print(str)
---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+//var str = "Hello, playground"
+//var numberOfStoplights = "Four"
+//var numberOfStoplights: Int = 4
+let numberOfStoplights: Int = 4
+//numberOfStoplights += 2
+var population: Int
+population = 5422
+let townName: String = "Knowhere"
+//let townDescription =
+//"\(townName) has a population of \(population) and \(numberOfStoplights) stoplights."
+//print(townDescription)
+
+var unemployment: Int
+unemployment = 12
+let townDescription =
+"\(townName) has a population of \(population) and \(numberOfStoplights) stoplights and unemployment is \(unemployment) %"
+print(townDescription)
